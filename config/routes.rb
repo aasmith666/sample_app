537b16e87468e4cd0ee1de049fd500f847ac0328
Allison Smith <aasmith4@matcmadison.edu>
2012-02-25 14:12:35 -0600

    Added a users controller and layout tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 SampleApp::Application.routes.draw do
   
-  root :to => 'pages#home'
-  match '/contact', :to => 'pages#contact'
-  match '/about', :to => 'pages#about'
-  match '/help', :to => 'pages#help'
+  root 			:to => 'pages#home'
+  match '/contact', 	:to => 'pages#contact'
+  match '/about', 	:to => 'pages#about'
+  match '/help', 	:to => 'pages#help'
+  match '/signup', 	:to => 'users#new'
+  
   #match '/contact', :to => 'pages#contact'
   #get "pages/home"
   #get "pages/contact"
