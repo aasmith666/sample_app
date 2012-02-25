320d84892704aae9d337e9f39a5a7749bedfad36
Allison Smith <aasmith4@matcmadison.edu>
2012-02-25 13:31:32 -0600

    Added named routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
-  get "pages/help"
+  
+  root :to => 'pages#home'
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
+  #match '/contact', :to => 'pages#contact'
+  #get "pages/home"
+  #get "pages/contact"
+  
+  #get "pages/about"
+ # get "pages/help"
+  
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -51,9 +62,7 @@ SampleApp::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  
 
   # See how all your routes lay out with "rake routes"
 
