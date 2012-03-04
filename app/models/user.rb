27ac9da9ca0326a78e46bcc49dcd14dacf66b3f1
Allison Smith <aasmith4@matcmadison.edu>
2012-03-04 11:27:19 -0600

    User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ActiveRecord::Base
   #ADD SECURITY
   attr_accessible :name, :email
+  
+  #Define regular expression variable for valid email address
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
   #ADD VALIDATION
-  validates :name, :presence => true,
-		   :length   => { :maximum => 50 }
-  validates :email, :presence => true
+  validates :name, :presence 	=> true,
+		   :length   	=> { :maximum => 50 }
+  validates :email, :presence 	=> true,
+		    :format	=> { :with => email_regex },
+		    :uniqueness	=> { :case_sensitive => false }
   
   
 end
