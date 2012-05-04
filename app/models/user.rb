cffc62fb8e697fba1874b457ae40f37090683610
Allison Smith <aasmith4@matcmadison.edu>
2012-05-03 20:29:36 -0500

    Added a relationships model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,14 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   
   #ADD ASSOCIATION
-  has_many :microposts, :dependent => :destroy
-  
+  has_many :microposts, 			:dependent => :destroy
+  has_many :relationships, 			:dependent => :destroy,
+									:foreign_key => "follower_id"
+  has_many :reverse_relationships, 	:dependent => :destroy,
+									:foreign_key => "followed_id",
+									:class_name => "Relationship"
+  has_many :following,	:through => :relationships, :source => :followed
+  has_many :followers,	:through => :reverse_relationships, :source => :follower
   #Define regular expression variable for valid email address
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
@@ -31,6 +37,18 @@ class User < ActiveRecord::Base
   
   def feed
 	 Micropost.where("user_id = ?", id) 
+  end
+  
+  def following?(followed)
+	 !!relationships.find_by_followed_id(followed) 
+  end
+  
+  def follow!(followed)
+	 relationships.create!(:followed_id => followed.id) 
+  end
+  
+  def unfollow!(followed)
+	 relationships.find_by_followed_id(followed).destroy 
   end
    
   class << self
--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -42,3 +42,15 @@ describe Micropost do
 	end
 	
 end
+
+# == Schema Information
+#
+# Table name: microposts
+#
+#  id         :integer         not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
